45eed7bc6bb8f19418e64e4dc34526a3bcdd0f16
vitoriasiqueira <vitoria.siqueira@estudante.ifms.edu.br>
2024-03-13 10:15:00 -0400

    Meu primeiro componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
+import MeuComponente from './componentes/MeuComponente'
 function App() {
 let nome = "Vitória"
 let sobrenome = "Dangui Siqueira"
  return (
+   <>
+   <MeuComponente></MeuComponente>
   <div>
      <h1 className="nome">Nome: {nome} </h1>
      <p className="sobrenome">{sobrenome}</p>
   </div>
+  </>
   )// Retorna JSX - JavaScript XML
 }
 export default App
